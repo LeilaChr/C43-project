79b618dcb78d483397a816c9a00e679c9e1dafc6
Ian Gregory <justcheesy@outlook.com>
2023-07-22 12:48:35 -0400

    Improve form feedback

--- mybnb/app.py
+++ mybnb/app.py
@@ -11,16 +11,15 @@ app.config['SECRET_KEY'] = 'hello'
 bootstrap = Bootstrap5(app)
 
 
-def form_endpoint(form, template_path: str, on_submit: callable = lambda: 0, next_location: str = '', template_args: dict = {}):
+def form_endpoint(form, template_path: str, on_submit: callable, next_location: str = None, template_args: dict = {}):
     if not isinstance(form, FlaskForm):
         # Instantiate form class
         form = form()
-    if not next_location:
-        next_location = request.path
 
     if form.validate_on_submit():
         on_submit(form)
-        return redirect(next_location)
+        if next_location:
+            return redirect(next_location)
 
     return render_template(template_path, form=form, **template_args)
 
@@ -32,19 +31,24 @@ def home():
 @app.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     class Form(FlaskForm):
-        name = StringField('Name',  validators=[DataRequired(), Length(0, 63)])
-        username = StringField('Choose a Username', validators=[DataRequired(), Length(0, 63)])
-        password = PasswordField('Choose a Password', validators=[DataRequired(), Length(0, 63)])
+        name = StringField('Name',  validators=[DataRequired(), Length(1, 63)])
+        username = StringField('Choose a Username', validators=[DataRequired(), Length(1, 63)])
+        password = PasswordField('Choose a Password', validators=[DataRequired(), Length(1, 63)])
         submit = SubmitField('Sign Up')
 
+    def on_submit(form):
+        # TODO: Write to database here
+        pass
+
     return form_endpoint(Form, 'sign-up.html',
-                         next_location='/dashboard')
+                         next_location='/dashboard',
+                         on_submit=on_submit)
 
 @app.route('/log-in', methods=['GET', 'POST'])
 def log_in():
     class Form(FlaskForm):
-        username = StringField('Username', validators=[DataRequired(), Length(0, 63)])
-        password = PasswordField('Password', validators=[DataRequired(), Length(0, 63)])
+        username = StringField('Username', validators=[DataRequired(), Length(1, 63)])
+        password = PasswordField('Password', validators=[DataRequired(), Length(1, 63)])
         submit = SubmitField('Log In')
 
     def on_submit(form):
@@ -57,7 +61,8 @@ def log_in():
         }
 
     return form_endpoint(Form, 'log-in.html', on_submit,
-                         next_location='/dashboard')
+                         next_location='/dashboard',
+                         on_submit=on_submit)
 
 @app.route('/dashboard')
 def dashboard():
@@ -68,12 +73,10 @@ def dashboard():
 def my_profile():
     class Form(FlaskForm):
         id = StringField('User ID', render_kw={'readonly': True})
-        name = StringField('Name',  validators=[DataRequired(), Length(0, 63)])
-        username = StringField('Username', validators=[DataRequired(), Length(0, 63)])
+        name = StringField('Name',  validators=[DataRequired(), Length(1, 63)])
+        username = StringField('Username', validators=[DataRequired(), Length(1, 63)])
         password = PasswordField('Password', validators=[Length(0, 63)])
         submit = SubmitField('Save Changes')
-
-    submitted = False
 
     def on_submit(form):
         # TODO: Write to database here
@@ -84,7 +87,6 @@ def my_profile():
             'name': form.name.data
         }
 
-        submitted = True
 
     form = Form()
     global user
@@ -92,8 +94,8 @@ def my_profile():
     form.name.data = user['name']
     form.username.data = user['username']
 
-    return form_endpoint(form, 'my-profile.html', on_submit,
-                         template_args={'submitted': submitted})
+    return form_endpoint(form, 'my-profile.html',
+                         on_submit=on_submit)
 
 
 if __name__ == '__main__':
