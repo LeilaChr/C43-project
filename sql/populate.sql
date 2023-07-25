7cfb6508bfae55b161de5b7914186ddd6611cd54
Ian Gregory <justcheesy@outlook.com>
2023-07-25 17:41:53 -0400

    Script that populates the database

---
+++ sql/populate.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO Users(id, sin, username, name, dob, address, occupation)
+VALUES (1, 111222333, 'ian', 'Ian Gregory', '2002-09-18', '123 Happy St, Scarborough, ON', 'Student');
+
+INSERT IGNORE INTO Users(id, sin, username, name, dob, address, occupation)
+VALUES (10, 777888999, 'bobby', 'Little Bobby Tables', '2019-01-02', '123 Happy St, Scarborough, ON', 'Kindergarten Student');
